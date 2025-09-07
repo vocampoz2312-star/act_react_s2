97422e878a584e7b8648d0ec31c78fb2cd210fa0
Vanessa <vocampoz2312@gmail.com>
2025-09-06 20:16:19 -0500

    Se crea componente con estilos globales

--- src/app/components/TarjetaGlobal.js
+++ src/app/components/TarjetaGlobal.js
@@ -1,15 +1,14 @@
-"use client"; // necesario si usas interactividad en Next.js
+"use client";
 
 import React from "react";
-import "../../globals.css"; // asegúrate de importar los estilos globales
 
 export default function TarjetaGlobal({ titulo, contenido, color }) {
-  return (
-    <div className="tarjeta-global" style={{ borderColor: color }}>
-      <h2 className="titulo-global" style={{ color: color }}>
-        {titulo}
-      </h2>
-      <p className="contenido-global">{contenido}</p>
-    </div>
-  );
+return (
+<div className="tarjeta-global" style={{ borderColor: color }}>
+<h2 className="titulo-global" style={{ color }}>
+{titulo}
+</h2>
+<p className="contenido-global">{contenido}</p>
+</div>
+);
 }
